d25df0597dab52ae27f2065093111a83efa0b6fd
albertorodzba <alberto_rdzbautista@hotmail.com>
2023-07-29 13:16:21 -0600

    [UPDATE]: contact us view

--- src/app/modules/home/pages/contact-us/contact-us.component.ts
+++ src/app/modules/home/pages/contact-us/contact-us.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class ContactUsComponent {
 
+  OnSubmit(): void{
+
+  }
+
+  ValidateFile(event: Event): void{
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    const selectedFile = input.files![0];
+    const sizeMB: number =  selectedFile.size / 1048576;//MEGABYTES
+    
+    
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const fileData = e.target.result.split(',')[1]; // Obtener solo la parte de Base64
+      console.log("base64");
+      console.log(fileData);
+    };
+
+    reader.readAsDataURL(selectedFile);
+  
+
+    console.log(selectedFile);
+    console.log(sizeMB + " MB")
+  }
 }
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -3,15 +3,51 @@ import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  models: string[] = ["PC54","G5000","ST350","G8000","PC54","G5000","ST350"];
+  models: string[] = [
+    'PC54',
+    'G5000',
+    'ST350',
+    'G8000',
+    'PC54',
+    'G5000',
+    'ST350',
+  ];
   windowWidth: number = window.innerWidth;
 
-  @HostListener("window:resize", ["$event"])
-  onWidthChange(): void{
+  apparel: string[] = [
+    'T-Shirts',
+    'Polo Shirts',
+    'Caps & Benies',
+    'Hoodies',
+    'Jackets',
+    'Safety Wear',
+  ];
+
+  marketingProducts: string[] = [
+    'Business Cards',
+    'Marketing Products',
+    'Postcards',
+    'Flyers & Brochures',
+    'Canvas',
+    'Car Magnets',
+    'Yard Signs',
+    'Label',
+  ];
+
+  grandFormat: string[] = [
+    'Signs',
+    'Grand Format',
+    'Banner stand',
+    'Windows Graphics',
+    'Stickers',
+    'Wall Decals',
+    'Posters',
+  ];
+  @HostListener('window:resize', ['$event'])
+  onWidthChange(): void {
     this.windowWidth = window.innerWidth;
   }
-
 }
